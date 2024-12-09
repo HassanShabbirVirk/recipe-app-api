9402721a200ebd2bb75c1ec022bc925022b8d358
HassanShabbirVirk <hassan.shabbir.lhr@gmail.com>
2024-12-09 18:50:14 +0500

    Commit

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
     to perform different mathematical operations on numbers
 """
 
+
 def add(x, y):
     """Add x and y to return results."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y to return results."""
--- app/app/test.py
+++ app/app/test.py
@@ -4,13 +4,15 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
+
     def test_add_number(self):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         res = calc.subtract(5, 6)
 
